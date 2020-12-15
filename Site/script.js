74c432c6cd626bb9022736dcf0ed63b56827cbc5
Santiago Thea <mac@MacBook-Pro-de-Santiago-Thea.local>
2020-12-15 01:01:48 -0300

    primer commit

---
+++ Site/script.js
@@ -0,0 +1,4 @@
+var pagina = 'segundaplaca-first-play.html';
+        var segundos = 10;
+        function redireccion() { document.location.href=pagina;}
+        setTimeout("redireccion()",segundos*1000);
